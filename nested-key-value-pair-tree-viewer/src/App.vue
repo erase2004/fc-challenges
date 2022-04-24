62119dc69d2cce606dc05a47333d262487d8a683
erase2004 <erase2004@gmail.com>
2022-04-25 00:25:59 +0800

    [Add] gap between input area and display area

--- nested-key-value-pair-tree-viewer/src/App.vue
+++ nested-key-value-pair-tree-viewer/src/App.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 div(class="md:px-20 px-2 pt-5 h-screen flex flex-col")
   h1(class="mb-1 text-2xl text-primary-200 font-bold") Nested Key-Value Pair Tree Viewer
-  div(class="mb-5 flex flex-grow flex-col lg:flex-row overflow-hidden")
+  div(class="mb-5 flex flex-grow flex-col lg:flex-row overflow-hidden gap-4")
     InputArea(:mounted="teleportTargetMounted")
     DisplayArea(@ready="setMounted")
 
