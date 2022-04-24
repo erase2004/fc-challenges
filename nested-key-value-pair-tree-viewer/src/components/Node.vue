81504627f18f3bdfb9248fb9d667cffdf1f98df1
erase2004 <erase2004@gmail.com>
2022-04-24 22:45:17 +0800

    [Update] change list style

--- nested-key-value-pair-tree-viewer/src/components/Node.vue
+++ nested-key-value-pair-tree-viewer/src/components/Node.vue
@@ -3,13 +3,13 @@ div(class="text-lg")
   //- node with children
   template(v-if="Object.keys(props.tree.children).length > 0")
     div(class="flex cursor-pointer")
-      span(class="font-bold whitespace-nowrap") {{props.tree.key}}
       span(
-        class="ml-1"
+        class="mr-1"
         @click.stop="toggle"
       ) [{{collapse === true ? '+' : '-'}}]
+      span(class="font-bold whitespace-nowrap") {{props.tree.key}}
     div(
-      class="pl-[1em]"
+      class="pl-[1.5em]"
       v-show="collapse === false"
     )
       Node(v-for="node in Object.values(props.tree.children)" :tree="node")
