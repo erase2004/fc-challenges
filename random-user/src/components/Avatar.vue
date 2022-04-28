a06ed20b1296b573335546175e431a920d2fd49c
erase2004 <erase2004@gmail.com>
2022-04-28 11:45:45 +0800

    [Add] Avatar component

---
+++ random-user/src/components/Avatar.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+div(class="w-32 h-32 rounded-full bg-slate-400")
+  img(
+    v-show="selfImageUrl !== ''"
+    :src="selfImageUrl"
+    @error="selfImageUrl = ''"
+    class="w-full h-full rounded-full object-cover"
+  )
+</template>
+
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+interface Props {
+  imageUrl: string;
+}
+
+const props = defineProps<Props>()
+const selfImageUrl = ref(props.imageUrl)
+
+watch(() => props.imageUrl, (value, previouseValue) => {
+  selfImageUrl.value = value
+})
+</script>
