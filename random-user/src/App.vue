c603d9d96e48e0c92e4eaebdb5e3d2dea5a818e2
erase2004 <erase2004@gmail.com>
2022-04-29 01:17:20 +0800

    [Add] listFormat sync to localStorage behavior

--- random-user/src/App.vue
+++ random-user/src/App.vue
@@ -1,3 +1,14 @@
 <template lang="pug">
 router-view
 </template>
+
+<script setup lang="ts">
+import storage from '@/utils/storage';
+import { useStore as useUIStore } from '@/stores/ui';
+
+const uiStore = useUIStore()
+uiStore.$subscribe((mutation, state) => {
+  storage.set('listFormat', state.listFormat)
+})
+
+</script>
