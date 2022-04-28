52060313f74a5dfcd6666a7798865b0256df7cd8
erase2004 <erase2004@gmail.com>
2022-04-28 10:54:02 +0800

    [Fix] gitignore config

--- random-user/src/main.ts
+++ random-user/src/main.ts
@@ -1,8 +1,8 @@
 import '@/styles/main.css'
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from '@/router/index'
+import { createPinia } from 'pinia'
 
 const app = createApp(App)
 app.use(router)
