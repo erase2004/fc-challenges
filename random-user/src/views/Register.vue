d93097437c02015c98f31e9450e912f7b17add54
erase2004 <erase2004@gmail.com>
2022-04-28 11:46:13 +0800

    [Add] Register page structure and style

---
+++ random-user/src/views/Register.vue
@@ -0,0 +1,43 @@
+<template lang="pug">
+div(class="inline-block m-auto p-10 self-center bg-[#dfe6ed]")
+  div(class="flex flex-col gap-y-6 ")
+    Avatar(
+      :imageUrl="imageUrl"
+      class="self-center"
+    )
+    input(
+      class="text-input"
+      placeholder="Email"
+      name="email"
+      type="email"
+    )
+    input(
+      class="text-input"
+      placeholder="Name"
+      name="name"
+      type="text"
+    )
+    input(
+      v-model="imageUrl"
+      class="text-input"
+      placeholder="Avatar Image URL"
+      name="avatar"
+      type="text"
+    )
+    input(
+      class="text-input"
+      placeholder="Password"
+      name="password"
+      type="password"
+    )
+    div(class="flex flex-row justify-between")
+      span(class="action-text-primary") Login
+      button(class="action-button-primary") Register
+</template>
+
+<script setup lang="ts">
+import Avatar from '@/components/Avatar.vue';
+import { ref } from 'vue'
+
+const imageUrl = ref<string>('')
+</script>
