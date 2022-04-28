bcb18ec4116fe4624c5079979e433dea414fb6ea
erase2004 <erase2004@gmail.com>
2022-04-29 00:23:27 +0800

    [Add] List component in UserList

---
+++ random-user/src/components/Users/List.vue
@@ -0,0 +1,53 @@
+<template lang="pug">
+div(class="flex flex-row h-full flex-shrink-1 flex-grow-0 overflow-x-clip overflow-y-auto")
+  div(
+    v-if="format === 'list'"
+    class="flex flex-col flex-grow gap-y-8"
+  )
+    div(
+      v-for="item in list"
+      :key="item"
+      class="flex flex-row flex-shrink-0 h-28 bg-substrate p-4 overflow-hidden"
+    )
+      Avatar(
+        imageUrl=""
+        class="h-20 w-20 flex-shrink-0"
+      )
+      div(class="flex flex-col flex-grow flex-shrink justify-center text-lg font-semibold leading-tight overflow-hidden px-4")
+        div(class="inline-block") aaaa bbbb cccc ddd eeee ffff aaaa bbbb cccc ddd eeee ffff aaaa bbbb cccc ddd eeee ffff
+      div(class="inline-block flex-shrink-0 w-6 h-6 ml-auto cursor-pointer")
+        HeartFill(class="fill-[#4b5c6b]")
+        HeartEmpty(class="fill-[#4b5c6b]")
+
+  div(
+    v-else
+    class="flex flex-row flex-wrap gap-x-6 gap-y-8")
+    div(
+      v-for="item in list"
+      :key="item"
+      class="inline-block w-40 h-60 bg-substrate p-4 overflow-hidden"
+    )
+      Avatar(imageUrl="")
+      div(
+        class="leading-tight overflow-hidden text-ellipsis line-clamp-[2] my-3 mx-auto"
+        title="aaaa bbbb cccc ddd eeee ffff"
+      ) aaaa bbbb cccc ddd eeee ffff
+      div(class="block w-6 h-6 ml-auto cursor-pointer")
+        HeartFill(class="fill-[#4b5c6b]")
+        HeartEmpty(class="fill-[#4b5c6b]")
+
+</template>
+
+<script setup lang="ts">
+import { ref, reactive } from 'vue'
+import Avatar from '@/components/Avatar.vue'
+import HeartFill from '@/components/icons/HeartFill.vue'
+import HeartEmpty from '../icons/HeartEmpty.vue'
+
+let list = reactive<number[]>([])
+let format = ref<string>('list')
+
+for (let n = 0; n < 50; n += 1) {
+  list.push(n)
+}
+</script>
--- random-user/src/views/UserList.vue
+++ random-user/src/views/UserList.vue
@@ -1,8 +1,9 @@
 <template lang="pug">
 div(class="flex flex-col h-full w-full")
   Header
-  div(class="flex flex-col flex-grow px-4")
+  div(class="flex flex-col flex-grow px-4 overflow-hidden")
     Control
+    List
     Pagination
 
 
@@ -11,6 +12,7 @@ div(class="flex flex-col h-full w-full")
 <script setup lang="ts">
 import Header from '@/components/Users/Header.vue'
 import Control from '@/components/Users/Control.vue'
+import List from '@/components/Users/List.vue'
 import Pagination from '@/components/Users/Pagination.vue'
 
 
