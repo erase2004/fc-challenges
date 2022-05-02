375d75123e89e99e2a2d37d876f338f0225643fc
erase2004 <erase2004@gmail.com>
2022-05-02 15:51:35 +0800

    [Update] Avatar error handle behavior

--- random-user/src/components/Avatar.vue
+++ random-user/src/components/Avatar.vue
@@ -1,23 +1,38 @@
 <template lang="pug">
 div(class="w-32 h-32 rounded-full bg-slate-400")
   img(
-    v-show="selfImageUrl !== ''"
+    v-if="selfImageUrl !== ''"
     :src="selfImageUrl"
-    @error="selfImageUrl = ''"
+    @error="handleError"
     class="w-full h-full rounded-full object-cover"
   )
 </template>
 
 <script setup lang="ts">
 import { ref, watch } from 'vue'
+
 interface Props {
   imageUrl: string;
 }
+interface Emits {
+  (e: 'failed'): void;
+}
 
 const props = defineProps<Props>()
+const emit = defineEmits<Emits>()
 const selfImageUrl = ref(props.imageUrl)
 
 watch(() => props.imageUrl, (value, previouseValue) => {
   selfImageUrl.value = value
 })
+
+function handleError() {
+  if (selfImageUrl.value === undefined) {
+    selfImageUrl.value = ''
+    return
+  }
+
+  selfImageUrl.value = ''
+  emit('failed')
+}
 </script>
