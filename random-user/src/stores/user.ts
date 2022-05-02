29cee8e223be75f79720b71949eb09e10b4428d6
erase2004 <erase2004@gmail.com>
2022-05-02 15:53:23 +0800

    [Update] ui store, add onAuthStateChanged handle

--- random-user/src/stores/user.ts
+++ random-user/src/stores/user.ts
@@ -1,21 +1,37 @@
 import { defineStore } from 'pinia'
 import storage from '@/utils/storage'
+import { auth } from '@/utils/firebase'
+import { onAuthStateChanged } from '@firebase/auth'
 
 const TOKEN_KEY = 'accessToken'
 
-export const useStore = defineStore('user', {
-  state: () => ({
-    'accessToken': storage.get(TOKEN_KEY) || undefined
-  }),
-  getters: {
+export const useStore = () => {
+  const s = defineStore('user', {
+    state: () => ({
+      'accessToken': storage.get(TOKEN_KEY) || undefined
+    }),
+    getters: {
 
-  },
-  actions: {
-    setToken(token: string) {
-      this.accessToken = token
-      storage.set(TOKEN_KEY, token)
+    },
+    actions: {
+      setToken(token: string) {
+        this.accessToken = token
+        storage.set(TOKEN_KEY, token)
+      }
     }
-  }
-})
+  })
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      // login
+
+    } else {
+      // logout
+
+    }
+  })
+
+  return s;
+}
 
 
