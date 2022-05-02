1c3f70f8e238c7617e4228181185501857b99812
erase2004 <erase2004@gmail.com>
2022-05-02 19:34:26 +0800

    [Add] name attribute in user data in initilization script

--- random-user/src/initialize.ts
+++ random-user/src/initialize.ts
@@ -14,6 +14,7 @@ async function initialize() {
   const mapUsers = users.map(user => ({
     uid: user.uid,
     email: user.email,
+    name: user.name,
     avatar: user.avatar,
     gender: user.gender,
     age: user.age
