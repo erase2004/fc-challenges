2c26cf57f4e59c5e8af0ceafd29a30aa3aab64f0
erase2004 <erase2004@gmail.com>
2022-05-02 14:02:52 +0800

    [Update] Add form validation and login handler in Login page

--- random-user/src/views/Login.vue
+++ random-user/src/views/Login.vue
@@ -1,23 +1,104 @@
 <template lang="pug">
 div(class="inline-block m-auto p-10 self-center bg-substrate")
-  div(class="flex flex-col gap-y-6 ")
-    input(
-      class="text-input"
-      placeholder="Email"
-      name="email"
-      type="email"
-    )
-    input(
-      class="text-input"
-      placeholder="Password"
-      name="password"
-      type="password"
-    )
+  form(
+    class="flex flex-col gap-y-4"
+    @submit.stop.prevent="handleSubmit"
+  )
+    div(class="w-56")
+      input(
+        class="text-input"
+        placeholder="Email"
+        name="email"
+        type="email"
+        v-model.lazy="email"
+        :disabled="isProcessing"
+      )
+      div(class="input-error") {{ errors.email }}
+    div(class="w-56")
+      input(
+        class="text-input"
+        placeholder="Password"
+        name="password"
+        type="password"
+        v-model.lazy="password"
+        :disabled="isProcessing"
+      )
+      div(class="input-error") {{ errors.password }}
     div(class="flex flex-row justify-between")
       span(class="action-text-primary") Register
-      button(class="action-button-primary") Login
+      button(
+        class="action-button-primary"
+        type="submit"
+        :disabled="meta.valid === false || isProcessing === true"
+      ) Login
 </template>
 
 <script setup lang="ts">
+import type { AuthError } from '@firebase/auth';
+import { useForm, useField } from 'vee-validate'
+import * as yup from 'yup'
+import authService from '@/services/auth'
+import { useStore } from '@/stores/user'
+import { ref } from 'vue'
+import { useRouter } from 'vue-router';
+
+const userStore = useStore()
+const router = useRouter()
+const isProcessing = ref<boolean>(false)
+
+const schema = yup.object({
+  email: yup.string().required().email(),
+  password: yup.string().required().min(6)
+})
+
+const { errors, meta, setFieldError } = useForm({
+  validationSchema: schema,
+});
+
+const { value: email } = useField<string>('email');
+const { value: password } = useField<string>('password');
+
+async function handleSubmit() {
+  if (meta.value.valid === false || isProcessing.value === true) {
+    return
+  }
+
+  isProcessing.value = true
+
+  try {
+    const user = await authService.signIn({
+      email: email.value,
+      password: password.value
+    })
+
+    const token = await user.getIdToken()
+    userStore.setToken(token)
+
+    router.push({'name': 'user-list'})
+
+  } catch (error: unknown) {
+    const { code } = error as AuthError
+
+    switch (code) {
+      case 'auth/invalid-email':
+        setFieldError('email', 'invalid email')
+        break;
+      case 'auth/user-disabled':
+        setFieldError('email', 'user disabled')
+        break;
+      case 'auth/user-not-found':
+        setFieldError('email', 'user doesn\'t exist')
+        break;
+      case 'auth/wrong-password':
+        setFieldError('password', 'wrong password')
+        break;
+      default:
+        console.error(error)
+        break;
+    }
+  } finally {
+    isProcessing.value = false
+  }
+}
 
 </script>
