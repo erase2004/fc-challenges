1edce26d069433ffa151694b70a20c45e37a4f44
erase2004 <erase2004@gmail.com>
2022-05-03 20:59:53 +0800

    [Fix] import path

--- random-user/src/main.ts
+++ random-user/src/main.ts
@@ -1,6 +1,6 @@
 import '@/styles/main.css'
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from '@/App.vue'
 import router from '@/router/index'
 import { createPinia } from 'pinia'
 
